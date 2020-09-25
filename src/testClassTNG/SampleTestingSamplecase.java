47e29c40ff4fa327d22e2ba8953e09f6b2841a53
nmalireddy <nmalireddy@NSUS-TAB03>
2020-09-25 15:40:15 -0400

    sample testing example

---
+++ src/testClassTNG/SampleTestingSamplecase.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package testClassTNG;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author nmalireddy
+ *
+ */
+public class SampleTestingSamplecase 
+{
+@Test
+public void sample()
+{
+	System.out.println("hello");
+}
+}
